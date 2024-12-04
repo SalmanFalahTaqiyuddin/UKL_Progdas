b5a516f5da636776054a9c8e1be2c855d0c01233
Salman Falah Taqiyuddin <salman_taqiyuddin_33rpl@student.smktelkom-mlg.sch.id>
2024-12-04 13:37:11 +0700

    Update soal12w.java

--- soal12w.java
+++ soal12w.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class soalMudah1 {
+public class soalMudah2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Masukkan angka: ");
