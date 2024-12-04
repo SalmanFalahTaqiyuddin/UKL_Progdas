2effe477510da841b3b0892f98b7059ee901b8a2
Salman Falah Taqiyuddin <salman_taqiyuddin_33rpl@student.smktelkom-mlg.sch.id>
2024-12-04 13:39:19 +0700

    Update soal21w.java

--- soal21w.java
+++ soal21w.java
@@ -1,8 +1,6 @@
-package Soalbeneran;
-
 import java.util.Scanner;
 
-public class soal21w {
+public class soalSedang1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner (System.in);
         
